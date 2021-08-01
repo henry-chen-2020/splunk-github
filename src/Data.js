28c305899258f00a76662801d049b26deaaebde7
henry-chen-2020 <40145021+henry-chen-2020@users.noreply.github.com>
2021-08-01 12:57:18 -0700

    update cache

--- src/Data.js
+++ src/Data.js
@@ -7,19 +7,15 @@ const TOKEN = {
 /**
  * A function to cache call to GitHub API
  */
-let splunkRepos;
 async function getSplunkRepos(per_page = 100) {
-  if (!splunkRepos) {
-    const pages = await getSplunkRepoPages(per_page);
-    const pageList = Array.from({ length: pages }, (v, i) => i + 1);
-    const requests = pageList.map(async (page) => {
-      const response = await fetch(getGitHubURL(per_page, page));
-      return await response.json();
-    });
-    const buffer = await Promise.all(requests);
-    splunkRepos = buffer.flat();
-  }
-  return splunkRepos;
+  const pages = await getSplunkRepoPages(per_page);
+  const pageList = Array.from({ length: pages }, (v, i) => i + 1);
+  const requests = pageList.map(async (page) => {
+    const response = await fetch(getGitHubURL(per_page, page));
+    return await response.json();
+  });
+  const buffer = await Promise.all(requests);
+  return buffer.flat();
 }
 
 function getGitHubURL(per_page, page = 1) {
@@ -37,23 +33,27 @@ async function getSplunkRepoPages(per_page = 100) {
 /**
  * get list of repos with required fields
  */
+let splunkRepos;
 function getGitHubRepoURL(repo) {
   return `https://api.github.com/repos/${SPLUNK}/${repo}/commits/HEAD`;
 }
 async function listSplunkRepos() {
-  const data = await getSplunkRepos();
-  const results = data.map(async (repo) => {
-    const response = await fetch(getGitHubRepoURL(repo.name), TOKEN);
-    const json = await response.json();
-    return {
-      key: repo.id,
-      name: repo.name,
-      author: json.author?.id ?? 0,
-      forks: repo.forks,
-      stars: repo.watchers
-    };
-  });
-  return Promise.all(results);
+  if (!splunkRepos) {
+    const data = await getSplunkRepos();
+    const results = data.map(async (repo) => {
+      const response = await fetch(getGitHubRepoURL(repo.name), TOKEN);
+      const json = await response.json();
+      return {
+        key: repo.id,
+        name: repo.name,
+        author: json.author?.id ?? 0,
+        forks: repo.forks,
+        stars: repo.watchers
+      };
+    });
+    splunkRepos = await Promise.all(results);
+  }
+  return splunkRepos;
 }
 
 export default listSplunkRepos;
